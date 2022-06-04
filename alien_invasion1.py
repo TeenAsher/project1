69c72cd5083a8c417bfa8e750b39b6716dfccd1e
TeenAsher <tanyushashilina1@gmail.com>
2022-06-04 12:13:20 +0300

    Second commit(the main loop of the game)

---
+++ alien_invasion1.py
@@ -0,0 +1,32 @@
+# ------------------------------------
+# First file of the project №1 'Alien Invasion'
+# 'Alien Invasion' - game from the book 'Python Crash Course' by Eric Matthes
+# Code by TeenAsher
+# ------------------------------------
+import pygame
+from pygame import *
+import sys
+
+class AlienInvasion:
+    """Overall class to manage game's assets and behavior"""
+
+    def __init__(self):
+        """Initialises the pygame and creates the game's resources"""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption('Alien Invasion')
+
+    def run_game(self):
+        """Start the main loop for the game"""
+        while True:
+
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
